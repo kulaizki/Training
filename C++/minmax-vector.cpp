d05624941f217a99ffb365cdc821276fda9678ca
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-27 20:12:38 +0800

    Update minmax-vector.cpp

--- C++/minmax-vector.cpp
+++ C++/minmax-vector.cpp
@@ -4,19 +4,27 @@
 
 using namespace std;
 
-int main()
-{
-    //vector declaration
-    vector<int> v1{ 10, 20, 30, 40, 50, 25, 15 };
-    int min = 0;
-    int max = 0;
-
-    //finding minimum  & maximum element
-    min = *min_element(v1.begin(), v1.end());
-    max = *max_element(v1.begin(), v1.end());
-
-    cout << "minimum element of the vector: " << min << endl;
-    cout << "maximum element of the vector: " << max << endl;
-
+int main() {
+    
+    int n, x, min = 0, max = 0;
+    cout << "Enter n: ";
+    cin >> n;
+    
+    vector<int> arr;
+    cout << "\nEnter elements:\n";
+    
+    while (n--) {
+        cin >> x;
+        arr.push_back(x);
+    }
+    
+    arr.clear(2);
+    
+    min = *min_element(arr.begin(), arr.end());
+    max = *max_element(arr.begin(), arr.end());
+    
+    cout << "Minimum element of vector is " << min << '\n';
+    cout << "Maximum element of vector is " << max << '\n';
+    
     return 0;
 }
