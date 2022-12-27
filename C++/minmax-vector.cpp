4d2725a7bc5784c46e4cbf560f9bd550d5294c94
kulaizki <91787757+kulaizki@users.noreply.github.com>
2022-12-27 18:12:22 +0800

    Create minmax-vector.cpp

---
+++ C++/minmax-vector.cpp
@@ -0,0 +1,20 @@
+//C++ STL program to append a vector to a vector
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //vector declaration
+    vector<int> v1{ 10, 20, 30, 40, 50, 25, 15 };
+    int min = 0;
+    int max = 0;
+
+    //finding minimum  & maximum element
+    min = *min_element(v1.begin(), v1.end());
+    max = *max_element(v1.begin(), v1.end());
+
+    cout << "minimum element of the vector: " << min << endl;
+    cout << "maximum element of the vector: " << max << endl;
+
+    return 0;
+}
