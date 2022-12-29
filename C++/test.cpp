ca16cac76cad70440ebb1d1396c0e59e8153b6ae
kulaizki <kulaizke@gmail.com>
2022-12-29 17:01:34 +0800

    practice

--- C++/test.cpp
+++ C++/test.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() 
+using namespace std;
+
+bool isAnagram(string s, string t) {
+    if (s.size() != t.size())
+        return false;
+
+    vector<int> h(26);
+
+    for (int i=0; i<s.size(); i++)
+        h[s[i] - 'a']++;
+
+    for (int i=0; i<t.size(); i++) {
+        int x = --h[t[i] - 'a'];
+        if (x < 0) return false;
+    }
+
+    return true;
+}
+
+int main()
 {
-    std::cout << "Hello, World";
+    string s = "hello", t = "elloh";
+
+    isAnagram(s, t) ? cout << "True" : cout << "False";
 
     return 0;
 }
