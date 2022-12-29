e58be64602fac426c9f1c3cff20bae228dc2fbbd
kulaizki <kulaizke@gmail.com>
2022-12-29 18:03:50 +0800

    umap

--- C++/umap.cpp
+++ C++/umap.cpp
@@ -2,25 +2,33 @@
 // functionality of unordered_map
 #include <iostream>
 #include <unordered_map>
+#include <map>
+
 using namespace std;
- 
+
 // Driver code
 int main()
 {
-  // Declaring umap to be of
-  // <string, int> type key
-  // will be of STRING type
-  // and mapped VALUE will
-  // be of int type
-  unordered_map<string, int> umap;
- 
-  // inserting values by using [] operator
-  umap["GeeksforGeeks"] = 10;
-  umap["Practice"] = 20;
-  umap["Contribute"] = 30;
- 
-  // Traversing an unordered map
-  for (auto x : umap)
-    cout << x.first << " " <<
-            x.second << endl;
+    // Declaring umap to be of
+    // <string, int> type key
+    // will be of STRING type
+    // and mapped VALUE will
+    // be of int type
+    unordered_map<int, int> ump;
+    map<char, int> mp;
+
+    // inserting values by using [] operator
+    ump[5] = 10;
+    ump[10] = 20;
+    ump[15] = 30;
+    mp['a'] = 123;
+    mp['b'] = 321;
+    mp['c'] = 132;
+
+    // Traversing 
+    for (auto x : ump)
+        cout << x.first << " " << x.second << '\n';
+
+    for (auto x : mp)
+        cout << x.first << " " << x.second << '\n';
 }
