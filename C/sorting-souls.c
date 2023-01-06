d7ee9a5cc686f47f4e50c08ad6896d0708a1c3a7
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-06 14:19:35 +0800

    Update sorting-souls.c

--- C/sorting-souls.c
+++ C/sorting-souls.c
@@ -8,6 +8,7 @@ int *sorting_souls(int arr[]);
 int main() {
     int list[MAX_SIZE];
 
+    // gets the number of elements
     printf("Enter number of souls: ");
     scanf("%d", &list[0]);
 
@@ -44,21 +45,26 @@ int *sorting_souls(int arr[]) {
         max2 = INT_MIN,
         ctr = 0;
     
+    // gets first highest number
     for (int i = 1; i <= arr[0]; i++) {
         if (arr[i] > max1)
             max1 = arr[i];
     }
+    // gets second highest number
     for (int i = 1; i <= arr[0]; i++) {
         if (arr[i] > max2 && arr[i] < max1) {
             max2 = arr[i];
-            ctr = i;
+            ctr = i; // gets the index of the second highest number
         }
     }
 
+    // creates a dynamic array
     int *sorted_souls = (int *)malloc(sizeof(int) * arr[0] - ctr+1),
         count = 1;
     sorted_souls[0] = arr[0] - ctr + 1;
     if (arr[0] == 1) sorted_souls[0] = 0;
+
+    // moves the elements to the dynamic array
     for (int i = ctr; i <= arr[0]; i++)
         sorted_souls[count++] = arr[i];
 
