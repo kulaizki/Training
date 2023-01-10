0ea381a60a7caccb0fbec45bfdbba5d64dac3b33
kulaizki <91787757+kulaizki@users.noreply.github.com>
2023-01-11 00:40:10 +0800

    Update maximum-value-with-count.cpp

--- C++/maximum-value-with-count.cpp
+++ C++/maximum-value-with-count.cpp
@@ -1,15 +1,31 @@
-class Solution {
-public:
-    vector<int> maxValNumOfOccurrences(vector<int>& nums) {
-        int maxVal = nums[0], count = 0;
-        for (int val: nums) {
-            if (val > maxVal) {
-                maxVal = val;
-                count = 1;
-            } else if (val == maxVal) {
-                count++;
-            }
-        }
-        return {maxVal, count};
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout << "Enter n: ";
+    int n;
+    cin >> n;
+    cout << "Enter elements: ";
+    vector<int> v;
+    while (n--) {
+        int x;
+        cin >> x;
+        v.push_back(x);
     }
-};
+
+    int maxVal = v[0];
+    int count = 0;
+    for (int i: v) {
+        if (i > maxVal) {
+            maxVal = i;
+            count = 1;
+        } else if (maxVal == i) {
+            count++;
+        }
+    }
+
+    cout << "The maximum value is " << maxVal << " and it appears " << count << " times.\n";
+
+    return 0;
+}
