2368ac74c75b26a013c03aadc7d2c9542b3e3bc3
kulaizki <kulaizke@gmail.com>
2023-02-03 13:09:33 +0800

    hashmap

---
+++ Python/twosum_improved.py
@@ -0,0 +1,24 @@
+def twosum(nums, target):
+    num_dict = {}
+    result = []
+    for i, num in enumerate(nums):
+        if target - num in num_dict:
+            result.append([num, target - num])
+        num_dict[num] = i
+    return result
+
+arr = []
+n = int(input("Enter n: "))
+
+for i in range(n):
+    element = int(input("Enter element: "))
+    arr.append(element)
+
+target = int(input("Enter target: "))
+twoArr = twosum(arr, target)
+
+if twoArr:
+    for i in twoArr:
+        print(i, end=" ")
+else:
+    print("No two elements in the array add up to the target sum")
