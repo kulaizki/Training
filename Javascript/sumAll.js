8dbbd1036da08ec4426242db99f1f0dc18345df9
kulaizki <kulaizke@gmail.com>
2023-02-10 11:08:41 +0800

    exercise

---
+++ Javascript/sumAll.js
@@ -0,0 +1,20 @@
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || isNaN(start) || isNaN(end) || 
+        typeof(start) != "number" || typeof(end) != "number")
+            return "ERROR";
+
+    let sum = 0;
+    let i = start;
+    let n = end;
+
+    if (start > end) {
+        i = end;
+        n = start;
+    }
+
+    for (;i <= n; i++) {
+        sum += i
+    }
+
+    return sum;
+};
