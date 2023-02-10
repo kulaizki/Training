6ab34ae071ce684b8291f8eadb2afd8cf768f0fc
kulaizki <kulaizke@gmail.com>
2023-02-10 11:10:01 +0800

    exercise

---
+++ Javascript/fibonacci.js
@@ -0,0 +1,14 @@
+const fibonacci = function(n) {
+    if (n < 0) 
+        return "OOPS";
+    if (typeof(n) === "string")
+        n = Number(n);
+
+    let n1 = 0, n2 = 1, sum = 1;
+    for (let i = 1; i < n; ++i) {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+    return sum;
+};
