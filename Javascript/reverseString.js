316c5e6ef0e5ff8b83008c467a534a6cd608b45f
kulaizki <kulaizke@gmail.com>
2023-02-10 11:08:13 +0800

    exercise

---
+++ Javascript/reverseString.js
@@ -0,0 +1,5 @@
+const reverseString = function(string) {
+    const array = string.split(""); // splits the characters, split(" ") splits words
+    const reverseArray = array.reverse();
+    return reverseArray.join(""); // concatenates all characters in the array into one string
+};
