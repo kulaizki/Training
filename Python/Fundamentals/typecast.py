d13e7b5a37eba5826ea38f59d9186f50031eca81
kulaizki <kulaizke@gmail.com>
2023-02-12 23:07:35 +0800

    typecast

---
+++ Python/Fundamentals/typecast.py
@@ -0,0 +1,10 @@
+print(str(0))
+print(int("-99"))
+print(int(1.25))
+print(float(10))
+
+# print(int("1.25")) 
+# (string to floating-point is not allowed)
+# this needs to be rounded first
+
+print(round(float("1.25")))
