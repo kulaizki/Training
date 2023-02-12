5fc137645017839a1857c2607b270fdb90be55ba
kulaizki <kulaizke@gmail.com>
2023-02-12 22:54:10 +0800

    progress

--- Python/General/sorting-souls_improved.py
+++ Python/General/sorting-souls_improved.py
@@ -1,18 +1,1 @@
 # in progress
-
-def sortSouls(arr):
-    if arr[0] < 2:
-        return []
-
-    return newArr
-
-souls = []
-n = int(input("Enter n: "))
-souls.append(n)
-
-for i in range(1, souls[0]+1):
-    element = int(input("Enter element: "))
-    souls.append(element)
-
-sorted_souls = sortSouls(souls)
-print(sorted_souls)
