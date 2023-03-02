7f2935af33bf019b0d7173aae91d800b7eba447d
kulaizki <kulaizke@gmail.com>
2023-03-02 08:26:05 +0800

    sort

--- C/Sorting/selection-sort.c
+++ C/Sorting/selection-sort.c
@@ -15,15 +15,17 @@ int main() {
 void selectionSort(int arr[], int n) {
 
     int i, j, min_index;
-    
+
     for (i = 0; i < n-1; ++i) {
         min_index = i;
         for (j = i+1; j < n; ++j) {
-            if (arr[j] < arr[min_index]) min_index = j;
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[min_index];
+            arr[min_index] = temp;
         }
-        int temp = arr[i];
-        arr[i] = arr[min_index];
-        arr[min_index] = temp;
     }
 
 }
