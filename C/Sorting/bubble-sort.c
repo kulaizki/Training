596fd50ed25ed462f4756cfa78287120e22ff2a6
kulaizki <kulaizke@gmail.com>
2023-03-02 08:23:21 +0800

    sort

--- C/Sorting/bubble-sort.c
+++ C/Sorting/bubble-sort.c
@@ -14,9 +14,9 @@ int main() {
 
 void bubbleSort(int arr[], int n) {
 
-    n--;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n-i; ++j) {
+    int i, j; n--;
+    for (i = 0; i < n; ++i) {
+        for (j = 0; j < n-i; ++j) {
             if (arr[j] > arr[j+1]) {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
