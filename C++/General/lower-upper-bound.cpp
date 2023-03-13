04e17f8e20db34d1f3a561785bcdfd6fb012e632
kulaizki <kulaizke@gmail.com>
2023-03-13 21:59:48 +0800

    v.begin(), v.end(), x

--- C++/General/lower-upper-bound.cpp
+++ C++/General/lower-upper-bound.cpp
@@ -10,9 +10,16 @@ int main()
  
     sort(v.begin(), v.end()); // 5 5 5 6 6 6 7 7
  
+    cout << "v.begin: " << *v.begin() << '\n';
+    cout << "v.end: " << *v.end() << '\n';
+
     vector<int>::iterator lower, upper;
+
     lower = lower_bound(v.begin(), v.end(), 6);
     upper = upper_bound(v.begin(), v.end(), 6);
+
+    cout << "lower: " << *lower << '\n';
+    cout << "upper: " << *upper << '\n';
 
     cout << "lower_bound for 6 at index "
          << (lower - v.begin()) << '\n';
